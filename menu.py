730f5c26a7037b4a57b81b7a77db6df1275948e3
GuilhermeB <guilhermebazani7@gmail.com>
2025-10-27 09:53:13 -0300

    menu do codigo

---
+++ menu.py
@@ -0,0 +1,18 @@
+.
+while True:
+    print("MENU PRINCIPAL")
+    print("1 - Cadastro")
+    print("2 - Login")
+    print("3 - Sair")
+
+    escolha = input("Escolha uma opção: ")
+
+    if escolha == "1":
+        cadastro()
+    elif escolha == "2":
+        login()
+    elif escolha == "3":
+        print("Encerrando o sistema. Até mais!")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
